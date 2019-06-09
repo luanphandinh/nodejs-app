ab9bca759b5f4fcfc123ef64a68ad79886c15b86
Luan Phan <luanphandinh@outlook.com>
2019-06-09 22:46:30 +0700

    merge handler tests into 1 file

--- tests/utils/Handlers.test.ts
+++ tests/utils/Handlers.test.ts
@@ -2,9 +2,10 @@ import { Response as TestResponse } from 'supertest';
 import { Request, Response } from "express";
 
 import app from '@tests/appTest';
-import { Err400, Err404 } from "@utils/Errors";
+import { Err400, Err404 } from '@utils/Errors';
+import Handlers from '@utils/Handlers';
 
-describe('utils/Errors', () => {
+describe('error', () => {
   it('should return 400 Bad Request', () => {
     return app
       .withRoute('/400', (req: Request, res: Response): Response => {
@@ -45,6 +46,34 @@ describe('utils/Errors', () => {
       .expect(500)
       .then((res: TestResponse) => {
         expect(res.body).toHaveProperty('message');
+      });
+  });
+});
+
+describe('async', () => {
+  it('should handle async response', () => {
+    return app
+      .withRoute('/async', Handlers.async(async (req: Request, res: Response): Promise<Response> => res.json({ async: 'work'})))
+      .listen()
+      .get('/async')
+      .expect(200)
+      .then((res: TestResponse) => {
+        expect(res.body).toEqual({ async: 'work'});
+      });
+  });
+
+  it('should handle async error correctly', () => {
+    return app
+      .withRoute('/async400', Handlers.async(async (req: Request, res: Response): Promise<Response> => {
+        throw new Err400('Invalid Params');
+      }))
+      .listen()
+      .get('/async400')
+      .expect(400)
+      .then((res: TestResponse) => {
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toBe('Invalid Params');
+        expect(res.body).toHaveProperty('errors');
       });
   });
 });
