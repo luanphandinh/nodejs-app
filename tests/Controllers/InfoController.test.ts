6b8eca90d97d3d1c2c96c6b67e3080036315ec29
Luan Phan <luan.phan@go1.com>
2019-06-09 13:47:03 +0700

    unit test

---
+++ tests/Controllers/InfoController.test.ts
@@ -0,0 +1,18 @@
+import { Response } from 'supertest';
+import app from '@tests/app';
+
+describe('GET /', () => {
+  it('should return 200 OK', () => {
+    return app
+      .listen()
+      .get('/')
+      .expect(200)
+      .then((res: Response) => {
+        expect(res.body).toHaveProperty('service');
+        expect(res.body).toHaveProperty('version');
+        expect(res.body).toHaveProperty('time');
+      });
+  });
+});
+
+afterEach(() => app.stop());
