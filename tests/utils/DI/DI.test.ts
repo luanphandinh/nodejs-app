d6b7b191c13a1e51242b694ccf3b5cc7b2654230
Luan Phan <luanphandinh@outlook.com>
2019-06-26 23:16:23 +0700

    Add Injectable() annotion, able to make class injectable into DI container

---
+++ tests/utils/DI/DI.test.ts
@@ -0,0 +1,14 @@
+import { injectable } from '@utils/DI/DI';
+import * as DI from '@utils/DI/DI';
+
+describe('DI', () => {
+  @injectable()
+  class HelloWorld {
+    public say = () => 'Hello World!';
+  }
+
+  it('should injectable class and resolve it', () => {
+    const container = DI.getContainer();
+    expect(container.get<HelloWorld>(HelloWorld.name).say()).toEqual('Hello World!');
+  });
+});
