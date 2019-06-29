0e64ca7cc45af55c510b355afa1bf78504c4503e
Luan Phan <luanphandinh@outlook.com>
2019-06-29 08:11:42 +0700

    add paramIndex to inject, identified whether inject propery or inject function parameter

--- utils/DI/DI.ts
+++ utils/DI/DI.ts
@@ -3,6 +3,7 @@ import { Container } from './Container';
 import { IContainer } from './IContainer';
 
 const DESIGN_TYPE = 'design:type';
+const DESIGN_PARAM_TYPES = 'design:paramtypes';
 const container: IContainer = new Container();
 
 function injectable(): ClassDecorator {
@@ -12,9 +13,14 @@ function injectable(): ClassDecorator {
 }
 
 function inject() {
-  return (target: any, propertyKey: string | symbol) => {
-    const type = Reflect.getMetadata(DESIGN_TYPE, target, propertyKey);
-    target[propertyKey] = container.get(type.name);
+  return (target: any, propertyKey: string | symbol, parameterIndex?: number) => {
+    if (parameterIndex === undefined) {
+      const type = Reflect.getMetadata(DESIGN_TYPE, target, propertyKey);
+      target[propertyKey] = container.get(type.name);
+    } else {
+      const types = Reflect.getMetadata(DESIGN_PARAM_TYPES, target, propertyKey);
+      const values = types.map((type: any) => container.get(type.name)); // Resolves later
+    }
   };
 }
 
